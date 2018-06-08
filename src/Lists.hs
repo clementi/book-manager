3472bc1b04f2133688cad0b0ddd59b56c4a36ec7
Jeffrey Pratt <jeff@metronomix.com>
2018-06-08 11:07:00 -0600

    Make wordsWhen an 'instance' of a generic function for lists

--- src/Lists.hs
+++ src/Lists.hs
@@ -1,5 +1,6 @@
 module Lists
   ( at
+  , breakOn
   ) where
 
 at :: [a] -> Int -> Maybe a
@@ -8,3 +9,8 @@ at xs index = if index >= length xs || index < 0
                  then Nothing
                  else Just (xs !! index)
 
+breakOn :: (a -> Bool) -> [a] -> [[a]]
+breakOn p xs = case dropWhile p xs of
+                 [] -> []
+                 xs' -> x : breakOn p xs''
+                   where (x, xs'') = break p xs'
--- src/Strings.hs
+++ src/Strings.hs
@@ -2,8 +2,7 @@ module Strings
   ( wordsWhen
   ) where
 
+import Lists
+
 wordsWhen :: (Char -> Bool) -> String -> [String]
-wordsWhen p s = case dropWhile p s of
-                  "" -> []
-                  s' -> w : wordsWhen p s''
-                    where (w, s'') = break p s'
+wordsWhen = breakOn
